2fba64dac22b0528cefc58d14e8670713ab881a8
rudgerylopes94 <63984559+rudgerylopes94@users.noreply.github.com>
2022-05-17 09:37:12 +0100

    properties added

---
+++ rudgery_S00214383_FinalExam/RentalProperty.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rudgery_S00214383_FinalExam
+{
+    //main class
+    public class RentalProperty
+    {
+        //properties
+        public int ID { get; set; }
+
+        public enum RentalType { House, Flat, Share }
+
+        public string  Location { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        
+
+    }
+}
